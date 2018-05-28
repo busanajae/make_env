62d233c561df05184822a4b3bf4261bc759baf5c
BusanAjae <busanajae@gmail.com>
2018-05-28 17:35:10 +0900

    Corrected minor stuffs

--- env_java.sh
+++ env_java.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # OpenJava remove
-sudo apt-get autoremove -y openjdk-8-jre
-sudo apt-get purge -y openjdk*
+#sudo apt-get autoremove -y openjdk-8-jre
+#sudo apt-get purge -y openjdk*
 
 # Oracle JDK
 sudo add-apt-repository ppa:webupd8team/java
@@ -11,10 +11,10 @@ sudo apt-get install -y oracle-java8-installer
 sudo apt-get install -y oracle-java8-set-default
 
 # Jenkins
-wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
-sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
-sudo apt-get update
-sudo apt-get install -y jenkins
+#wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
+#sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+#sudo apt-get update
+#sudo apt-get install -y jenkins
 
 # Confirm
 clear
