f7e073848acfc6692534a4333682c00742ec3246
BusanAjae <busanajae@gmail.com>
2018-05-29 06:28:40 +0900

    Added addtional gcc tools installation scripts

--- env_gcc.sh
+++ env_gcc.sh
@@ -7,5 +7,14 @@ sudo apt-get install synaptic
 ### aptitude 패키지 관리자
 #sudo apt install aptitude
 
-### gcc
+### c & cpp compiler
 sudo apt install gcc
+sudo apt install g++
+
+# cmake 환경변수 추가
+sudo apt install automake
+sudo apt-get install cmake
+
+echo 'export CC=/usr/bin/gcc' >> ~/.bashrc
+echo 'export CXX=/usr/bin/g++' >> ~/.bashrc
+
